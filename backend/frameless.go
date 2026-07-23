a64727c8e88379bc501a39f43c7d149c2cad416b
hizyyo <kapolol2266@gmail.com>
2026-07-23 23:12:10 +0500

    acrylic blur + glassmorphism UI (fix background)
    - Use opaque dark bg + CSS glassmorphism on cards/tabs/search
    - DwmSetWindowAttribute + SetWindowCompositionAttribute fallback
    - Style clean-up for overlay look

--- backend/frameless.go
+++ backend/frameless.go
@@ -17,8 +17,8 @@ var (
 	procGetClientRect     = user32Frame.NewProc("GetClientRect")
 	procSetWindowRgn      = user32Frame.NewProc("SetWindowRgn")
 	procCreateRectRgn     = gdi32Frame.NewProc("CreateRectRgn")
-	procInvalidateRect    = user32Frame.NewProc("InvalidateRect")
 	procMoveWindow        = user32Frame.NewProc("MoveWindow")
+	procDwmSetWindowAttribute = dwmapiFrame.NewProc("DwmSetWindowAttribute")
 	procSetWindowCompositionAttribute = user32Frame.NewProc("SetWindowCompositionAttribute")
 	procDwmExtendFrameIntoClientArea  = dwmapiFrame.NewProc("DwmExtendFrameIntoClientArea")
 )
@@ -32,28 +32,45 @@ const (
 	WS_MINIMIZEBOX   = 0x00020000
 	WS_MAXIMIZEBOX   = 0x00010000
 
-	WS_EX_TOOLWINDOW = 0x00000080
-	WS_EX_APPWINDOW  = 0x00040000
-	WS_EX_WINDOWEDGE = 0x00000100
-	WS_EX_CLIENTEDGE = 0x00000200
-	WS_EX_STATICEDGE = 0x00020000
-	WS_EX_DLGMODALFRAME = 0x00000001
+	WS_EX_TOOLWINDOW   = 0x00000080
+	WS_EX_APPWINDOW    = 0x00040000
+	WS_EX_WINDOWEDGE   = 0x00000100
+	WS_EX_CLIENTEDGE    = 0x00000200
+	WS_EX_STATICEDGE    = 0x00020000
+	WS_EX_LAYERED       = 0x00080000
+	WS_EX_TRANSPARENT   = 0x00000020
 
 	SWP_FRAMECHANGED = 0x0020
 	SWP_NOMOVE       = 0x0002
 	SWP_NOSIZE       = 0x0001
 	SWP_NOZORDER     = 0x0004
 	SWP_NOACTIVATE   = 0x0010
-	SWP_SHOWWINDOW   = 0x0040
 )
 
 var (
 	gwlStyleVal   = uintptr(^uint32(15))
 	gwlExStyleVal = uintptr(^uint32(19))
+
+	DWMWA_SYSTEMBACKDROP_TYPE = uintptr(38)
+	DWMSBT_ACRYLIC            = uintptr(4)
+	DWMSBT_MAINWINDOW         = uintptr(2)
 )
 
 type RECT struct {
 	left, top, right, bottom int32
+}
+
+type ACCENTPOLICY struct {
+	AccentState   uint32
+	AccentFlags   uint32
+	GradientColor uint32
+	AnimationID   uint32
+}
+
+type WINCOMPATTRDATA struct {
+	Attribute int32
+	Data      uintptr
+	DataSize  uint32
 }
 
 func SetFramelessOverlay(title string) {
@@ -68,8 +85,8 @@ func SetFramelessOverlay(title string) {
 
 		var wr RECT
 		procGetWindowRect.Call(hwnd, uintptr(unsafe.Pointer(&wr)))
-		winW := wr.right - wr.left
-		winH := wr.bottom - wr.top
+		winW := uintptr(wr.right - wr.left)
+		winH := uintptr(wr.bottom - wr.top)
 
 		style, _, _ := procGetWindowLongW.Call(hwnd, gwlStyleVal)
 		removeStyle := uintptr(WS_CAPTION | WS_BORDER | WS_DLGFRAME | WS_THICKFRAME |
@@ -77,64 +94,69 @@ func SetFramelessOverlay(title string) {
 		procSetWindowLongW.Call(hwnd, gwlStyleVal, style & ^removeStyle)
 
 		exStyle, _, _ := procGetWindowLongW.Call(hwnd, gwlExStyleVal)
-		newEx := (exStyle | WS_EX_TOOLWINDOW) & ^uintptr(WS_EX_APPWINDOW | WS_EX_WINDOWEDGE | WS_EX_CLIENTEDGE)
+		newEx := exStyle | WS_EX_TOOLWINDOW | WS_EX_LAYERED
+		newEx = newEx & ^uintptr(WS_EX_APPWINDOW | WS_EX_WINDOWEDGE | WS_EX_CLIENTEDGE)
 		procSetWindowLongW.Call(hwnd, gwlExStyleVal, newEx)
 
-		procSetWindowPos.Call(hwnd, 0, 0, 0, uintptr(winW), uintptr(winH),
+		procSetWindowPos.Call(hwnd, 0, 0, 0, winW, winH,
 			SWP_FRAMECHANGED|SWP_NOMOVE|SWP_NOZORDER|SWP_NOACTIVATE)
 
 		var cr RECT
 		procGetClientRect.Call(hwnd, uintptr(unsafe.Pointer(&cr)))
-		cw := cr.right - cr.left
-		ch := cr.bottom - cr.top
-
-		hRgn, _, _ := procCreateRectRgn.Call(0, 0, uintptr(cw), uintptr(ch))
+		cw := uintptr(cr.right - cr.left)
+		ch := uintptr(cr.bottom - cr.top)
+		hRgn, _, _ := procCreateRectRgn.Call(0, 0, cw, ch)
 		if hRgn != 0 {
 			procSetWindowRgn.Call(hwnd, hRgn, 1)
 		}
-		_ = hRgn
 
-		enableAcrylic(hwnd)
+		enableAcrylicDWM(hwnd)
 
 		procShowWindow.Call(hwnd, SW_SHOW)
 	}()
 }
 
-type ACCENTPOLICY struct {
-	AccentState   uint32
-	AccentFlags   uint32
-	GradientColor uint32
-	AnimationID   uint32
-}
-
-type WINCOMPATTRDATA struct {
-	Attribute    int32
-	Data        uintptr
-	DataSize    uint32
-}
-
-func enableAcrylic(hwnd uintptr) {
-	if procSetWindowCompositionAttribute.Find() != nil {
-		return
+func enableAcrylicDWM(hwnd uintptr) {
+	var attr uint32 = 4
+	if procDwmSetWindowAttribute.Find() == nil {
+		ret, _, _ := procDwmSetWindowAttribute.Call(
+			hwnd, 38,
+			uintptr(unsafe.Pointer(&attr)),
+			4,
+		)
+		if ret == 0 {
+			return
+		}
 	}
 
-	accent := ACCENTPOLICY{
-		AccentState:   4,
-		AccentFlags:   0,
-		GradientColor: 0x22FFFFFF,
-		AnimationID:   0,
+	if procSetWindowCompositionAttribute.Find() == nil {
+		accent := ACCENTPOLICY{
+			AccentState:   4,
+			AccentFlags:   0,
+			GradientColor: 0x19000000,
+			AnimationID:   0,
+		}
+		data := WINCOMPATTRDATA{
+			Attribute:    19,
+			Data:         uintptr(unsafe.Pointer(&accent)),
+			DataSize:     uint32(unsafe.Sizeof(accent)),
+		}
+		ret, _, _ := procSetWindowCompositionAttribute.Call(hwnd, uintptr(unsafe.Pointer(&data)))
+		if ret != 0 {
+			return
+		}
+		accent.GradientColor = 0x1A000000
+		data.Attribute = 20
+		ret, _, _ = procSetWindowCompositionAttribute.Call(hwnd, uintptr(unsafe.Pointer(&data)))
+		if ret != 0 {
+			return
+		}
 	}
 
-	data := WINCOMPATTRDATA{
-		Attribute:    20,
-		Data:        uintptr(unsafe.Pointer(&accent)),
-		DataSize:    uint32(unsafe.Sizeof(accent)),
+	if procDwmExtendFrameIntoClientArea.Find() == nil {
+		margins := [4]int32{-1, -1, -1, -1}
+		procDwmExtendFrameIntoClientArea.Call(hwnd, uintptr(unsafe.Pointer(&margins[0])))
 	}
-
-	procSetWindowCompositionAttribute.Call(
-		hwnd,
-		uintptr(unsafe.Pointer(&data)),
-	)
 }
 
 type WindowMover struct {
@@ -153,7 +175,7 @@ func (wm *WindowMover) Move(x, y int) {
 	}
 	var r RECT
 	procGetWindowRect.Call(wm.hwnd, uintptr(unsafe.Pointer(&r)))
-	w := r.right - r.left
-	h := r.bottom - r.top
-	procMoveWindow.Call(wm.hwnd, uintptr(x), uintptr(y), uintptr(w), uintptr(h), 1)
+	w := uintptr(r.right - r.left)
+	h := uintptr(r.bottom - r.top)
+	procMoveWindow.Call(wm.hwnd, uintptr(x), uintptr(y), w, h, 1)
 }
